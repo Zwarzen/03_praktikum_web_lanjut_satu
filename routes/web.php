7cb6a006e7f1fc11e92c8d20bc2ba4c34ce162dc
Risang <56747783+Zwarzen@users.noreply.github.com>
2021-02-24 10:15:26 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProgramController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +19,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// 1. Route Biasa
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// 2 & 4. Route Prefix
+Route::prefix('/category')->group(function(){
+    Route::get('product', [ProductController::class, 'index'])->name('product');
+    Route::get('program', [ProgramController::class, 'index'])->name('program');
+});
+
+// 3. Route Param
+Route::get('/news/{title}', [NewsController::class, 'index'])->name('news');
+
+// 4. Route Biasa
+Route::get('/about-us', [AboutController::class, 'index'])->name('about-us');
+
+// 5. Route Resource
+Route::resource('/contact-us', ContactController::class, [
+    'only' => ['index']
+]);
